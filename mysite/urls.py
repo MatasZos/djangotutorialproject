4906f00d5c5727890957a92f70cbf3301ab74cbe
MatasZos <B00163194@mytudublin.ie>
2026-03-17 19:11:14 +0000

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
 from django.contrib import admin
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect("polls:index")
 
 urlpatterns = [
+    path("", home),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
-] 
+]
