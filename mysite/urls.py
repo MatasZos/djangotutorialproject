9215715289573af8931896792b6714b196c6dfbe
MatasZos <B00163194@mytudublin.ie>
2026-03-18 16:27:05 +0000

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,8 +6,7 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("""
         <h1>Django Tutorial Project</h1>
-        <p><a href="/polls/">Go to Polls</a></p>
-        <p><a href="/admin/"> Login as an Admin </a></p>
+        <p><a href="/polls/">Django Tutorial Polls</a></p>
         <p><a href="https://github.com/MatasZos/djangotutorialproject">GitHub with Source Code</a></p>
     """)
 
