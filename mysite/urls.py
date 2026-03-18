a770167f78192051b9f372e01e9f9f3900ecff76
MatasZos <B00163194@mytudublin.ie>
2026-03-18 16:20:01 +0000

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,9 +6,9 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("""
         <h1>Django Tutorial Project</h1>
-        <p>This is my deployed Django project.</p>
         <p><a href="/polls/">Go to Polls</a></p>
-        <p><a href="https://github.com/MatasZos/djangotutorialproject">View Source Code on GitHub</a></p>
+        <p><a href="/admin/"> Login as an Admin </a></p>
+        <p><a href="https://github.com/MatasZos/djangotutorialproject">GitHub with Source Code</a></p>
     """)
 
 urlpatterns = [
